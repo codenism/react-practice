5ebe547788b93de2275326b3ebe32eebb0882afa
Denis Miroshnichenko <denmrsh@gmail.com>
2019-11-17 21:11:46 +0300

    init

---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class QuizCreator extends Component {
+  render() {
+    return (
+        <div>
+            <h1>Quiz Creator</h1>
+        </div>
+    )
+  }
+}
+export default QuizCreator
