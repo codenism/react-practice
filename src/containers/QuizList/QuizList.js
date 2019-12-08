10a70f0b3d0e4b5c28b8d75d5c477a7b18c8ef40
Denis <denmrsh@gmail.com>
2019-12-08 20:57:51 +0300

    router + validate

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react'
+import classes from './QuizList.css'
+import {NavLink} from 'react-router-dom'
 
 class QuizList extends Component {
+
+  renderQuizes() {
+    return [1, 2, 3].map((quiz, index) => {
+      return (
+        <li
+        key={index}
+        >
+          <NavLink to={'/quiz/' + quiz}>
+            Тест {quiz}
+          </NavLink>
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
+      <div className={classes.QuizList}>
         <div>
             <h1>Quiz List</h1>
+            <ul>
+              { this.renderQuizes() }
+            </ul>
         </div>
+      </div>
     )
   }
 }
